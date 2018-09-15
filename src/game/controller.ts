bc2d1093c4303cd5317ca6d2015ccb096fdcaaef
urbbru <timothyd28@hotmail.com>
2018-09-15 16:35:38 +0200

    make sure only 1 move per request

--- src/game/controller.ts
+++ src/game/controller.ts
@@ -2,6 +2,11 @@ import {JsonController, Get, Post, Put, Body, Param, HttpCode, NotFoundError, Ba
 import Game from './entity';
 const data = require('./data')
 
+const moves = (board1, board2) => board1
+                        .map((row, y) => row.filter((cell, x) => board2[y][x] !== cell))
+                        .reduce((a, b) => a.concat(b))
+                        .length
+
 @JsonController()
 export default class GameController {
 
@@ -18,9 +23,11 @@ export default class GameController {
     ) {
       if(Object.keys(game).length > 1) throw new BadRequestError('you can only specify 1 value, which is ->name<-')
       if(!game.hasOwnProperty('name')) throw new BadRequestError('you can only specify ->name<-')
+
       const randieColor = data.colors[Math.floor(Math.random() * data.colors.length)]
+
       game.color = randieColor
-      game.board = data.defaultBoard
+
       return game.save()
     }
 
@@ -35,6 +42,10 @@ export default class GameController {
 
       const game = await Game.findOne(id)
       if(!game) throw new NotFoundError('Game not found djais')
+
+      if(update.board) {
+        if(moves(update.board, game.board) > 1) throw new BadRequestError(`you can only make 1 move per reuqest, current board is ${game.board[0]} - ${game.board[1]} - ${game.board[2]}`)
+      }
 
       return Game.merge(game, update).save()
     }
--- src/game/entity.ts
+++ src/game/entity.ts
@@ -1,17 +1,24 @@
 import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm'
 import { BaseEntity } from 'typeorm/repository/BaseEntity'
+import { IsString } from 'class-validator';
+const data = require('./data')
 
 @Entity()
 export default class Game extends BaseEntity {
     @PrimaryGeneratedColumn()
-    id:number;
+    id?:number;
 
-    @Column("text")
+    @IsString()
+    @Column("text", {nullable:false})
     name:string;
 
     @Column("text")
     color:string;
 
-    @Column("json")
+    @Column("json", {
+        array: true,
+        default: data.defaultBoard,
+        nullable: false
+    })
     board:string[][];
 }
